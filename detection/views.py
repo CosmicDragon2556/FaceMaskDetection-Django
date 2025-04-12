6f26e557d4ea7ad769ee3b642c4718b878adb5e4
WebWizard2965 <ahmadfaraaz2625@gmail.com>
2025-04-13 02:23:17 +0530

    Fix Render deployment

--- detection/views.py
+++ detection/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+# # Create your views here.
+
+from django.http import StreamingHttpResponse, JsonResponse
+from django.views.decorators import gzip
+import cv2
+import numpy as np
+from .utils.mask_detection import FaceMaskDetector
+
+detector = FaceMaskDetector()  # Your detection class
+
+def gen_frames():
+    camera = cv2.VideoCapture(0)
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            frame = detector.process_frame(frame)
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@gzip.gzip_page
+def video_feed(request):
+    return StreamingHttpResponse(gen_frames(), content_type="multipart/x-mixed-replace;boundary=frame")
