05abc15b8d9cadce65678b65a422391338527e45
WebWizard2965 <ahmadfaraaz2625@gmail.com>
2025-04-13 11:00:06 +0530

    Some Changes

--- detection/views.py
+++ detection/views.py
@@ -26,3 +26,24 @@ def gen_frames():
 @gzip.gzip_page
 def video_feed(request):
     return StreamingHttpResponse(gen_frames(), content_type="multipart/x-mixed-replace;boundary=frame")
+
+# views.py
+from detection.models.mask_model import build_model
+from detection.utils.data_loader import DataLoader
+from detection.utils.detector import FaceMaskDetector
+from detection.utils.trainer import ModelTrainer
+
+def train_view(request):
+    # 1. Load data
+    df = DataLoader.load_dataset('images/', 'annotations/')
+    DataLoader.prepare_cnn_dataset(df, 'images/')
+    
+    # 2. Train model
+    model = build_model()
+    class_weights = ModelTrainer.get_class_weights(
+        {'with_mask':3232, 'without_mask':717, 'mask_weared_incorrect':123}
+    )
+    ModelTrainer.train_model(model, 'dataset_cnn', class_weights)
+    
+    # 3. Save trained model
+    model.save('detection/models/trained_model.h5')
